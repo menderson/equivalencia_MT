aec9ebcc60bb4075a907a84e1a0d160f723e9953
menderson <menderson07@gmail.com>
2021-03-25 20:17:42 -0300

    inicio MTFDI -> MT

--- main.py
+++ main.py
@@ -97,14 +97,128 @@ def converte_para_duplamente_infinita(linhas, alfabeto_da_fita):
     arquivo.close()
 
 
-def converte_para_sipser(linhas):
-    print('a')
+def converte_para_sipser(linhas, alfabeto_da_fita):
+    # cria o arquivo ou substitui se já existir
+    arquivo = open('saida.txt', 'w')
+    arquivo.write((";I\n"))
+    arquivo.close()
+
+    # abre o aquivo em modo apend
+    arquivo = open('saida.txt', 'a')
+
+    # cria estados auxiliares para fazer o deslocamento até o final da fita
+    aux1 = "aux1\n"
+    aux2 = "aux2\n"
+    aux3 = "aux3\n"
+    aux4 = "aux4\n"
+    aux5 = "aux5\n"
+
+    # tuplas auxiliares para deslocar todos os simbolos umas vez para a direita
+    tupla_aux = ["0", "0", "¢", "r", aux1]
+    escreve(tupla_aux, arquivo)
+
+    tupla_aux = ["0", "1", "¢", "r", aux2]
+    escreve(tupla_aux, arquivo)
+
+    tupla_aux = [aux1.rstrip('\n'), "0", "0", "r", aux1]
+    escreve(tupla_aux, arquivo)
+    tupla_aux = [aux1.rstrip('\n'), "1", "0", "r", aux2]
+    escreve(tupla_aux, arquivo)
+
+    tupla_aux = [aux2.rstrip('\n'), "1", "1", "r", aux2]
+    escreve(tupla_aux, arquivo)
+    tupla_aux = [aux2.rstrip('\n'), "0", "1", "r", aux1]
+    escreve(tupla_aux, arquivo)
+
+    tupla_aux = [aux1.rstrip('\n'), "_", "0", "r", aux3]
+    escreve(tupla_aux, arquivo)
+    tupla_aux = [aux2.rstrip('\n'), "_", "1", "r", aux3]
+    escreve(tupla_aux, arquivo)
+    tupla_aux = ["0", "_", "_", "r", aux3]
+    escreve(tupla_aux, arquivo)
+
+    # tupla para inserir simbolo de final da palavra de entrada §
+    tupla_aux = [aux3.rstrip('\n'), "_", "§", "l", aux4]
+    escreve(tupla_aux, arquivo)
+
+    # tuplas para voltar o cabeçote para a posição inicial
+    tupla_aux = [aux4.rstrip('\n'), "0", "0", "l", aux4]
+    escreve(tupla_aux, arquivo)
+    tupla_aux = [aux4.rstrip('\n'), "1", "1", "l", aux4]
+    escreve(tupla_aux, arquivo)
+
+    # o estado inicial agora se chamará aux5
+    tupla_aux = [aux4.rstrip('\n'), "¢", "¢", "r", aux5]
+    escreve(tupla_aux, arquivo)
+
+    # trocar todas as menções de estado "0" por "aux5"
+    linhas_modificadas = []
+    for linha in linhas:
+        tupla = linha.split(' ')
+        # linhas não vazias
+        if tupla[0] != '\n':
+            if tupla[0] == '0':
+                tupla[0] = "aux5"
+            if tupla[4].rstrip('\n') == '0':
+                tupla[4] = "aux5\n"
+        linhas_modificadas.append(tupla)
+
+    # iniciamos a ler as tuplas de entrada
+    for linha in linhas_modificadas:
+        tupla = linha
+        # se nao for linha em branco
+        if tupla[0] != '\n':
+            # se o movimento for a esquerda
+            if tupla[3] == 'l':
+                # estado auxiliar para verificar se estamos no inicio da fita
+                tupla_aux = tupla
+                estado_destino = tupla_aux[4]
+                tupla_aux[4] = "aux" + tupla_aux[4]
+                estado_aux_copia = tupla_aux[4]
+                escreve(tupla_aux, arquivo)
+
+                # se ler o caracater que indica o inicio da fita
+                tupla_aux = [tupla_aux[4].rstrip(
+                    '\n'), "¢", "¢", "r", "estado_auxiliar"]
+                escreve(tupla_aux, arquivo)
+
+                alfabeto_da_fita.append("¢")
+                alfabeto_da_fita.append("§")
+                # desloca simbolos da fita para a direita
+                for simbolo in alfabeto_da_fita:
+                    #estado_aux = estado_aux_copia + simbolo
+                    tupla_aux = ["estado_auxiliar", simbolo,
+                                 "_", "r", estado_aux_copia.rstrip("\n") + simbolo + "\n"]
+                    escreve(tupla_aux, arquivo)
+                    for simbolo_aux in alfabeto_da_fita:
+                        tupla_aux = [estado_aux_copia.rstrip("\n") + simbolo, simbolo_aux,
+                                     simbolo, "r", estado_aux_copia.rstrip('\n') + simbolo_aux + "\n"]
+
+                # <current state> <current symbol> <new symbol> <direction> <new state>
+
+                # se ler qualquer outro simbolo, simula o movimento estacionario
+                tupla_aux2 = tupla
+                tupla_aux2[4] = "aux2" + tupla_aux[4]
+                estado_aux2 = tupla_aux2[4]
+                for simbolo in alfabeto_da_fita:
+                    tupla_aux = [estado_aux_copia.rstrip(
+                        '\n'), simbolo, simbolo, "r", estado_aux2]
+                    escreve(tupla_aux, arquivo)
+                    tupla_aux = [estado_aux2.rstrip(
+                        '\n'), simbolo, simbolo, "l", estado_destino]
+                    escreve(tupla_aux, arquivo)
+
+            else:
+                escreve(tupla, arquivo)
+
+    # <current state> <current symbol> <new symbol> <direction> <new state>
 
 
 def main():
 
     # recebe nome do arquivo de entrada
     # nome_arquivo = input("Digite o nome do arquivo de entrada com extensão\n")
+
     nome_arquivo = "entrada.txt"
 
     # lê o arquivo
@@ -117,7 +231,7 @@ def main():
         converte_para_duplamente_infinita(linhas, alfabeto_fita)
 
     elif ";I" in tipo:
-        converte_para_sipser(linhas)
+        converte_para_sipser(linhas, alfabeto_fita)
     else:
         print("Tipo invalido")
         exit()
