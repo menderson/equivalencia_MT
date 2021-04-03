310dee6b4437acc01763970022d02a51e532d17a
menderson <menderson07@gmail.com>
2021-04-03 19:43:22 -0300

    refatoraçao e readme

--- README.md
+++ README.md
@@ -1,11 +1,62 @@
-# equivalencia_MT
-Trabalho final da disciplina de Teoria da Computação.  
+<h1 align="center">Equivalencia de duas variantes de Maquinas de Turing<h1>
 
+<h2 align="center">Trabalho final da disciplina de Teoria da Computação </h2>
+<br/>
+
+O objetivo deste trabalho é demonstrar a equivalência de duas variantes disponíveis no 
+simulador online de máquinas de Turing em http://morphett.info/turing/turing.html
+Mais especificamente: será possível notar que a variante de fita duplamente infinita e  a variante de fita semi-infinita (modelo de Sipser) podem simular uma à outra.
+
+## Requisitos
+
+Python3 instalado na máquina
+
+## Para executar:
+### Na pasta do projeto:
+
+```bash
+$ python3 main.py <nome_do_arquivo_de_entrada_com extensão>
+```
+
+### Exemplo:
+```bash
+$ python3 main.py entrada.txt
+```
+
+## Saída
+
+O programa de Maquina de Turing equivalente estará salvo no arquivo "saida.txt"
+
+<br />
+
+## Enunciado do trabalho
+
+Trabalho Prático
 O objetivo deste trabalho é demonstrar a equivalência de duas variantes disponíveis no 
 simulador online de máquinas de Turing em http://morphett.info/turing/turing.html
 Mais especificamente: será possível notar que a variante de fita duplamente infinita e 
 a variante de fita semi-infinita (modelo de Sipser) podem simular uma à outra.
 
+O trabalho consistirá em um programa que receberá um arquivo txt com uma entrada para o simulador, 
+onde a primeira linha será
 
-¢ marca o inicio da fita
+;I
+caso seja um programa para máquina de Turing com fita duplamente infinita
 
+;S
+caso seja um programa para máquina de Turing com fita semi-infinita (modelo de Sipser)
+
+O restante do arquivo será constituído exclusivamente de linhas no formato
+    <current state> <current symbol> <new symbol> <direction> <new state>
+conforme especificado no site do simulador, sem uso de novos comentários.
+
+Tem-se a garantia de que os símbolos £,¢,§ não serão utilizados em nenhum arquivo de entrada, podendo ser utilizados como eventuais símbolos auxiliares.
+
+O programa deverá retornar como saída um arquivo txt com um programa equivalente para o simulador online com tipo de fita inverso ao indicado no arquivo de entrada.
+
+Os arquivos de entrada serão apenas de máquinas de Turing determinísticas e com codificação válida para o simulador. Todos os programas dados como entrada serão para reconhecimento de linguagens sobre o alfabeto {0,1}. Note, portanto, que a máquina de Turing dada como saída também será um reconhecedor para essa mesma linguagem sobre o alfabeto {0,1}.
+O programa deve ser possível de ser executado em um computador com sistema operacional Ubuntu 20.04.2 LTS de 64 bits. A linguagem de implementação é livre dentro destas restrições.
+A entrega deverá ser feita via moodle, com o programa final, instruções claras de execução caso necessárias e endereço de repositório público no github com todo o código-fonte.
+
+
+<h4 align="center"> <em>&lt;/&gt;</em> by <a href="https://github.com/menderson" target="_blank">mendersoncosta</a> </h4>
--- main.py
+++ main.py
@@ -1,52 +1,59 @@
+# Desenvolvido por: Menderson Nicolau Costa
+
+import sys
+
 
 def le_arquivo(nome_arquivo):
-    f = open(nome_arquivo, 'r')
-    tipo = f.readline()
-    linhas = f.readlines()
-    return tipo, linhas
+    try:
+        f = open(nome_arquivo, 'r')
+        tipo = f.readline()
+        linhas = f.readlines()
+        return tipo, linhas
+    except:
+        print("Erro ao abrir o arquivo")
+        exit()
+
+# função que acrescenta uma tuplas a lista de tuplas de saida
 
 
-def escreve(linha, arquivo):
-    arquivo.write((linha[0] + ' ' + linha[1] + ' ' + linha[2] +
-                   ' ' + linha[3] + ' ' + linha[4]))
+def acresenta(linha, tuplas):
+    tupla = (linha[0] + ' ' + linha[1] + ' ' +
+             linha[2] + ' ' + linha[3] + ' ' + linha[4])
+
+    tuplas.append(tupla)
+
+# função que retorna os simbolos da fita
 
 
 def alfabeto_da_fita(linhas):
-    # <current state> <current symbol> <new symbol> <direction> <new state>
     alfabeto_fita = ['_']
 
+    # percorre todas as tuplas procurando por simbolos diferentes nas segundas e terceiras posições
     for linha in linhas:
+
         tupla = linha.split(' ')
+
         # linhas não vazias
         if tupla[0] != '\n':
-            if tupla[1] in alfabeto_fita:
-                continue
-            else:
+
+            if tupla[1] not in alfabeto_fita:
                 alfabeto_fita.append(tupla[1])
 
-            if tupla[2] in alfabeto_fita:
-                continue
-            else:
+            if tupla[2] not in alfabeto_fita:
                 alfabeto_fita.append(tupla[2])
+
     return alfabeto_fita
 
 
-def converte_para_duplamente_infinita(linhas, alfabeto_da_fita):
+def converte_para_duplamente_infinita(linhas, alfabeto_da_fita, tuplas):
 
-    # cria o arquivo ou substitui se já existir
-    arquivo = open('saida.txt', 'w')
-    arquivo.write((";I\n"))
-    arquivo.close()
-
-    # abre o aquivo em modo apend
-    arquivo = open('saida.txt', 'a')
+    tuplas.append(";I\n")
 
     # cria estados auxiliares para inserir "¢" como marcador do inicio da fita
-    first_step = ["0", "*", "*", "l", "firstaux\n"]
-    second_step = ["firstaux", "_", "¢", "r", "second_aux\n"]
-
-    escreve(first_step, arquivo)
-    escreve(second_step, arquivo)
+    tupla = ["0", "*", "*", "l", "firstaux\n"]
+    acresenta(tupla, tuplas)
+    tupla = ["firstaux", "_", "¢", "r", "second_aux\n"]
+    acresenta(tupla, tuplas)
 
     linhas_modificadas = []
     # as demais menções ao estado "0" agr devem ser chamadas de  "second_aux"
@@ -64,19 +71,19 @@ def converte_para_duplamente_infinita(linhas, alfabeto_da_fita):
         tupla = linha
         # se nao for linha em branco
         if tupla[0] != '\n':
+            # para os movimentos a esquerda
             if tupla[3] == 'l':
                 tupla_aux = tupla
                 estado_destino = tupla_aux[4]
                 tupla_aux[4] = "aux" + tupla_aux[4]
                 estado_aux_copia = tupla_aux[4]
-                escreve(tupla_aux, arquivo)
+                acresenta(tupla_aux, tuplas)
 
                 # se ler o caracater que indica o inicio da fita
                 tupla_aux = [tupla_aux[4].rstrip(
                     '\n'), "¢", "¢", "r", estado_destino]
-                escreve(tupla_aux, arquivo)
+                acresenta(tupla_aux, tuplas)
 
-                # <current state> <current symbol> <new symbol> <direction> <new state>
                 # se ler qualquer outro simbolo, simula o movimento estacionario
                 tupla_aux2 = tupla
                 tupla_aux2[4] = "aux2" + tupla_aux[4]
@@ -84,27 +91,18 @@ def converte_para_duplamente_infinita(linhas, alfabeto_da_fita):
                 for simbolo in alfabeto_da_fita:
                     tupla_aux = [estado_aux_copia.rstrip(
                         '\n'), simbolo, simbolo, "r", estado_aux2]
-                    escreve(tupla_aux, arquivo)
+                    acresenta(tupla_aux, tuplas)
                     tupla_aux = [estado_aux2.rstrip(
                         '\n'), simbolo, simbolo, "l", estado_destino]
-                    escreve(tupla_aux, arquivo)
-
-                # escreve(aux2, arquivo)
+                    acresenta(tupla_aux, tuplas)
 
             else:
-                escreve(tupla, arquivo)
-
-    arquivo.close()
+                acresenta(tupla, tuplas)
 
 
-def converte_para_sipser(linhas, alfabeto_da_fita):
-    # cria o arquivo ou substitui se já existir
-    arquivo = open('saida.txt', 'w')
-    arquivo.write((";S\n"))
-    arquivo.close()
+def converte_para_sipser(linhas, alfabeto_da_fita, tuplas):
 
-    # abre o aquivo em modo apend
-    arquivo = open('saida.txt', 'a')
+    tuplas.append(";S\n")
 
     # cria estados auxiliares para fazer o deslocamento até o final da fita
     aux1 = "aux1\n"
@@ -117,43 +115,43 @@ def converte_para_sipser(linhas, alfabeto_da_fita):
     if "*" in alfabeto_da_fita:
         alfabeto_da_fita.remove("*")
 
-    # tuplas auxiliares para deslocar todos os simbolos umas vez para a direita
+    # tuplas auxiliares para deslocar todos os simbolos do alfabeto de entrada umas vez para a direita
     tupla_aux = ["0", "0", "¢", "r", aux1]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     tupla_aux = ["0", "1", "¢", "r", aux2]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     tupla_aux = [aux1.rstrip('\n'), "0", "0", "r", aux1]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
     tupla_aux = [aux1.rstrip('\n'), "1", "0", "r", aux2]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     tupla_aux = [aux2.rstrip('\n'), "1", "1", "r", aux2]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
     tupla_aux = [aux2.rstrip('\n'), "0", "1", "r", aux1]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     tupla_aux = [aux1.rstrip('\n'), "_", "0", "r", aux3]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
     tupla_aux = [aux2.rstrip('\n'), "_", "1", "r", aux3]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
     tupla_aux = ["0", "_", "_", "r", aux3]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     # tupla para inserir simbolo de final da palavra de entrada §
     tupla_aux = [aux3.rstrip('\n'), "_", "§", "l", aux4]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     # tuplas para voltar o cabeçote para a posição inicial
     tupla_aux = [aux4.rstrip('\n'), "0", "0", "l", aux4]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
     tupla_aux = [aux4.rstrip('\n'), "1", "1", "l", aux4]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     # o estado inicial agora se chamará aux5
     tupla_aux = [aux4.rstrip('\n'), "¢", "¢", "r", aux5]
-    escreve(tupla_aux, arquivo)
+    acresenta(tupla_aux, tuplas)
 
     # trocar todas as menções de estado "0" por "aux5"
     linhas_modificadas = []
@@ -167,12 +165,6 @@ def converte_para_sipser(linhas, alfabeto_da_fita):
                 tupla[4] = "aux5\n"
         linhas_modificadas.append(tupla)
 
-    # ATE AQUI OK
-    ####################
-    ###################################
-    ############################################
-    # controle para nao inserir varios estados de voltar iguais
-    estado_voltar_criado = 0
     # iniciamos a ler as tuplas de entrada
     for linha in linhas_modificadas:
         tupla = linha
@@ -180,126 +172,101 @@ def converte_para_sipser(linhas, alfabeto_da_fita):
         if tupla[0] != '\n':
             # se o movimento for a esquerda
             if tupla[3] == 'l':
-                # captura o estado destino e vai para um estado auxiliar
+
+                # captura o estado destino e cria um estado auxiliar
                 estado_destino = tupla[4].rstrip("\n")
                 tupla[4] = estado_destino + "auxinicio\n"
-                escreve(tupla, arquivo)
+                acresenta(tupla, tuplas)
 
                 # se o simbolo lido nao for o de inicio da fita, simula o movimento estacionario e vai para o estado destino
                 for simbolo in alfabeto_da_fita:
                     if simbolo != "*" and simbolo != "¢":
                         tupla = [estado_destino + "auxinicio", simbolo,
                                  simbolo, "r", estado_destino + "estadoauxnotbegin\n"]
-                        escreve(tupla, arquivo)
+                        acresenta(tupla, tuplas)
                 tupla = [estado_destino + "estadoauxnotbegin", "*",
                          "*", "l", estado_destino + "\n"]
-                escreve(tupla, arquivo)
+                acresenta(tupla, tuplas)
 
                 # tupla para verificar se o simbolo lido é o de inicio da fita ¢
                 tupla_aux = [estado_destino + "auxinicio", "¢",
                              "¢", "r", estado_destino + "auxinicio2\n"]
-                escreve(tupla_aux, arquivo)
-                # tupla_aux = [estado_destino + "auxinicio1", "*",
-                #             "*", "l", estado_destino + "auxinicio2\n"]
-                #escreve(tupla_aux, arquivo)
+                acresenta(tupla_aux, tuplas)
 
-                # acresenta simbolo final ao alfabeto da fita
+                # acresenta simbolo final e inicial ao alfabeto da fita
                 if "§" not in alfabeto_da_fita:
                     alfabeto_da_fita.append("§")
                 if "¢" not in alfabeto_da_fita:
                     alfabeto_da_fita.append("¢")
-
-                ####################################################################################
-                ####################################################################################
-                ####################################################################################
-                ####################################################################################
 
                 # faz o deslocamento de todos os elementos da fita uma casa para a direita
                 estado_auxiliar = estado_destino + "auxinicio2"
                 for simbolo in alfabeto_da_fita:
 
                     estado_auxiliar_simbolo = estado_destino + "auxinicio2" + simbolo
-                    if simbolo != "ASAS":
-                        tupla = [estado_auxiliar, simbolo,
-                                 "_", "r", estado_auxiliar_simbolo + "\n"]
-                        escreve(tupla, arquivo)
-
-                    else:
-                        tupla = [estado_auxiliar, simbolo,
-                                 simbolo, "r", estado_auxiliar_simbolo + "\n"]
-                        escreve(tupla, arquivo)
-
-                    # AQUI COMEÇA O PROBLEMA
+                    tupla = [estado_auxiliar, simbolo,
+                             "_", "r", estado_auxiliar_simbolo + "\n"]
+                    acresenta(tupla, tuplas)
 
                     for simbolo_aux in alfabeto_da_fita:
                         if simbolo_aux != "§":
                             tupla = [estado_auxiliar_simbolo, simbolo_aux, simbolo,
                                      "r", estado_destino + "auxinicio2" + simbolo_aux + "\n"]
-                            escreve(tupla, arquivo)
+                            acresenta(tupla, tuplas)
                         else:
-                            # continue
                             tupla = [estado_auxiliar_simbolo, simbolo_aux, simbolo,
                                      "r", "estado_auxiliar_voltar_final" + estado_destino + "\n"]
-                            escreve(tupla, arquivo)
+                            acresenta(tupla, tuplas)
                             tupla = ["estado_auxiliar_voltar_final" + estado_destino, "*", "§",
                                      "l", "estado_auxiliar_voltar" + estado_destino + "\n"]
-                            escreve(tupla, arquivo)
+                            acresenta(tupla, tuplas)
 
-                ####################################################################################
-                ####################################################################################
-                ####################################################################################
-                ####################################################################################
-                ####################################################################################
-
-                # FUNCIONANDO pra achar o inicio
+                # tuplas para achar o inicio
                 alfabeto_da_fita.remove("§")
-                if estado_voltar_criado == 0:
-                    for simbolo in alfabeto_da_fita:
-                        if simbolo != "¢":
-                            tupla = ["estado_auxiliar_voltar" + estado_destino, simbolo,
-                                     simbolo, "l", "estado_auxiliar_voltar" + estado_destino + "\n"]
-                            escreve(tupla, arquivo)
-                        else:
-                            tupla = ["estado_auxiliar_voltar" + estado_destino, simbolo,
-                                     simbolo, "r", estado_destino + "\n"]
-                            escreve(tupla, arquivo)
-                    #estado_voltar_criado = 1
+                for simbolo in alfabeto_da_fita:
+                    if simbolo != "¢":
+                        tupla = ["estado_auxiliar_voltar" + estado_destino, simbolo,
+                                 simbolo, "l", "estado_auxiliar_voltar" + estado_destino + "\n"]
+                        acresenta(tupla, tuplas)
+                    else:
+                        tupla = ["estado_auxiliar_voltar" + estado_destino, simbolo,
+                                 simbolo, "r", estado_destino + "\n"]
+                        acresenta(tupla, tuplas)
 
-            # Movimento de final da fita, daqui pra baixo ta funcionando
+            # movimento para a direita
             else:
                 # captura o estado destino e vai para um estado auxiliar
                 estado_destino = tupla[4].rstrip("\n")
                 tupla[4] = estado_destino + "auxfinal\n"
-                escreve(tupla, arquivo)
+                acresenta(tupla, tuplas)
 
                 # tupla para verificar se o simbolo lido é o de final da fita §, apaga o simbolo e insere branco
                 tupla_aux = [estado_destino + "auxfinal", "§",
                              "_", "r", estado_destino + "auxfinal2\n"]
-                escreve(tupla_aux, arquivo)
+                acresenta(tupla_aux, tuplas)
 
                 # escreve o simbolo final e vai para a esquerda
                 tupla_aux = [estado_destino + "auxfinal2",
                              "_", "§", "l", estado_destino + "\n"]
-                escreve(tupla_aux, arquivo)
+                acresenta(tupla_aux, tuplas)
 
                 # se o simbolo lido nao for o de final da fita, simula o movimento estacionario e vai para o estado destino
                 for simbolo in alfabeto_da_fita:
                     tupla = [estado_destino + "auxfinal", simbolo,
                              simbolo, "l", estado_destino + "estadoauxnotend\n"]
-                    escreve(tupla, arquivo)
+                    acresenta(tupla, tuplas)
                 tupla = [estado_destino + "estadoauxnotend", "*",
                          "*", "r", estado_destino + "\n"]
-                escreve(tupla, arquivo)
-    arquivo.close()
-    # <current state> <current symbol> <new symbol> <direction> <new state>
+                acresenta(tupla, tuplas)
 
 
 def main():
 
-    # recebe nome do arquivo de entrada
-    # nome_arquivo = input("Digite o nome do arquivo de entrada com extensão\n")
-
-    nome_arquivo = "entrada.txt"
+    # confere numero de argumentos
+    if len(sys.argv) != 2:
+        print("Numero de argumentos invalido")
+        exit()
+    nome_arquivo = sys.argv[1]
 
     # lê o arquivo
     tipo, linhas = le_arquivo(nome_arquivo)
@@ -307,28 +274,34 @@ def main():
     # encontra alfabeto da fita
     alfabeto_fita = alfabeto_da_fita(linhas)
 
+    # lista de tuplas convertidas
+    tuplas = []
+
+    equivalente = None
+    # chama as funções de conversão
     if ";S" in tipo:
-        converte_para_duplamente_infinita(linhas, alfabeto_fita)
+        equivalente = "Maquina de Turing com fita duplamente infinita"
+        converte_para_duplamente_infinita(linhas, alfabeto_fita, tuplas)
 
     elif ";I" in tipo:
-        converte_para_sipser(linhas, alfabeto_fita)
+        equivalente = "Maquina de Turing com fita infinita a direita (Sipser)"
+        converte_para_sipser(linhas, alfabeto_fita, tuplas)
     else:
-        print("Tipo invalido")
+        print("O tipo informado na primeira linha do arquivo de entrada é invalido\n")
         exit()
 
-    # Remover possiveis linhas duplicadas
-    nome_arq = "saida.txt"
-    tip, lines = le_arquivo(nome_arq)
-
+    # remove possiveis linhas duplicadas
     new_lines = []
-    for line in lines:
+    for line in tuplas:
         if line not in new_lines:
             new_lines.append(line)
 
-    output_file = "output.txt"
+    # escreve no arquivo de saida
+    output_file = "saida.txt"
     f = open(output_file, 'w')
-    f.write(tip)
     f.write("".join(new_lines))
+
+    print("Programa de " + equivalente + ' equivalente salva em "saida.txt"')
 
 
 if __name__ == "__main__":
